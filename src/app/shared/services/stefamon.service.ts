704b0068e665cb444b303c01464d1eb80ca1e7ad
Matheus Goncalves <math3usgoncalves@protonmail.com>
2023-02-26 16:34:40 -0300

    feat: Iniciando Stefamon service

--- src/app/shared/services/stefamon.service.ts
+++ src/app/shared/services/stefamon.service.ts
@@ -1,15 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment.prod';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment.prod";
+import { Observable } from "rxjs";
+import { Stefamon } from "../objects/Stefamon";
 
+@Injectable({
+  providedIn: "root",
+})
 export class StefamonService {
+  private readonly API = `${environment.urlBackend}/stefamon`;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   list(): Observable<any> {
-    return this.http.get(`${environment.urlBackend}/stefamon`);
+    return this.http.get<Stefamon[]>(`${this.API}/todos`);
   }
 
+  buscarItemPorId(id: number) {
+    return this.http.get<Stefamon>(`${this.API}/${id}`);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlBackend: 'http://hostlocal:0808/api'
+  urlBackend: 'http://localhost:8080/api'
 };
